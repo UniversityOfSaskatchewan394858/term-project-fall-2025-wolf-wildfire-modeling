bd157e6b6e54b0b983c17c489b7b5a00f14c7e27
Jehan <jehan.kashem@gmail.com>
2025-11-21 21:42:14 -0600

    added water cells based on elevation

--- WolfWildfireSimulation/_alp/Agents/Main/Code/Functions.java
+++ WolfWildfireSimulation/_alp/Agents/Main/Code/Functions.java
@@ -1,9 +1,15 @@
 double setInitialCellProperties()
 {/*ALCODESTART::1763779415620*/
+int i = 1;
 for (Cell c : cells) {
 	c.elevation = Math.random();
-	int greenValue = (int)Math.round(c.elevation * 255);
-	c.colour = new Color(0, greenValue, 0);
+	int colourValue = (int)Math.round(c.elevation * 200);
+	if (i % 2 == 0){
+	c.colour = new Color(0, colourValue, 0);
+	}
+	else{c.colour = new Color(0, 0, colourValue);}
+	c.vegetation = Math.random();
+	i++;
   }
   
 /*ALCODEEND*/}
