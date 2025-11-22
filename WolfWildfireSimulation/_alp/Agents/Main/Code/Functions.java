52d23f6b52bfdccec17f518a004b8c74b3294080
Jehan <jehan.kashem@gmail.com>
2025-11-21 22:01:47 -0600

    testing getX and getY for cells

--- WolfWildfireSimulation/_alp/Agents/Main/Code/Functions.java
+++ WolfWildfireSimulation/_alp/Agents/Main/Code/Functions.java
@@ -2,12 +2,28 @@ double setInitialCellProperties()
 {/*ALCODESTART::1763779415620*/
 int i = 1;
 for (Cell c : cells) {
+
+	/*
+	// testing for getX and getY
+	int x = (int)c.getX();
+	int y = (int)c.getY();
+	
+	c.colour = new Color(100 / (x+1) , 100 / (y+1), 0);
+	*/
+	
+	// elevation 
 	c.elevation = Math.random();
 	int colourValue = (int)Math.round(c.elevation * 200);
+	
+
+	// cell is water or land depending on elevation
+	// will be changed to reflect + or - with real data
 	if (i % 2 == 0){
 	c.colour = new Color(0, colourValue, 0);
 	}
 	else{c.colour = new Color(0, 0, colourValue);}
+
+	
 	c.vegetation = Math.random();
 	i++;
   }
