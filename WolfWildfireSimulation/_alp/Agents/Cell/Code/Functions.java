e0dcc256a9a2f4ca016e3edd0392e0acf1fc0b98
Deathray <acn3263827@gmail.com>
2025-11-23 22:50:23 -0600

    removed the entire temperature system due to it being non-functional when the scale of the geometry was taken into account. replaced it with a cellular based system to spread fire based on simulated per cell fire spread.

--- WolfWildfireSimulation/_alp/Agents/Cell/Code/Functions.java
+++ WolfWildfireSimulation/_alp/Agents/Cell/Code/Functions.java
@@ -1,130 +1,87 @@
-double temperatureRegulation()
-{/*ALCODESTART::1763781827539*/
-double angledWeight = 1;
-if (get_Main().directionalWeighting) { 
-	angledWeight = 1 / Math.sqrt(2);
-}
+boolean isInRadius(int i)
+{/*ALCODESTART::1763947322041*/
+double CellX = 0;
+double CellY = 0;
 
-// OOB stands for Out Of Bounds and this only impacts the world border
-double OOB_Temperature = temperature;
-if (get_Main().edgeHeatSink) { 
-	OOB_Temperature = get_Main().baseTemperature;
-}
+switch (i) { 
+	case 0:
+		CellX = -1;
+		CellY = 1;
+		break;
+	case 1:
+		CellY = 1;
+		break;
+	case 2:
+		CellX = 1;
+		CellY = 1;
+		break;
+	case 3:
+		CellX = 1;
+		break;
+	case 4:
+		CellX = 1;
+		CellY = -1;
+		break;
+	case 5:
+		CellY = -1;
+		break;
+	case 6:		
+		CellX = -1;
+		CellY = -1;
+		break;
+	case 7:		
+		CellX = -1;
+		break;
+}		
 
-Cell[] Neighbors = new Cell[8];
+CellX *= get_Main().cellWidth / 2;
+CellY *= get_Main().cellWidth / 2;
 
-Neighbors[0] = (Cell) this.getAgentNextToMe(NORTH);
-Neighbors[1] = (Cell) this.getAgentNextToMe(NORTHEAST);
-Neighbors[2] = (Cell) this.getAgentNextToMe(EAST);
-Neighbors[3] = (Cell) this.getAgentNextToMe(SOUTHEAST);
-Neighbors[4] = (Cell) this.getAgentNextToMe(SOUTH);
-Neighbors[5] = (Cell) this.getAgentNextToMe(SOUTHWEST);
-Neighbors[6] = (Cell) this.getAgentNextToMe(WEST);
-Neighbors[7] = (Cell) this.getAgentNextToMe(NORTHWEST);
+double vector = Math.pow(CellX - firePointX, 2) + Math.pow(CellY - firePointY, 2);
 
-double budget = 0;
-double denominator = 0;
-
-// pre-calculate how much temperature needs to be spread to
-// neighbours to avoid going all in on the first cell looked at
-// and evenly distribute heat as needed.
-for (int i = 0; i < 8; i++) {
-	double differance = temperature;
-	if (Neighbors[i] != null) {
-		differance -= Neighbors[i].temperature;
-	} else {
-		differance -= OOB_Temperature;
-	} 
-	
-	if (differance <= 0) { 
-		continue; // cell is same temperature so no need to change it
-	} 
-	
-	if (i % 2 == 0) { 
-		denominator += differance;
-	} else { // odd numbers are diagonal and subject to the angleWeight
-		denominator += angledWeight * differance; 
-	}
-}
-
-if (denominator <= 1e-12) {
-	return; // all cells surrounding it are the same temperature
-}
-
-budget = get_Main().woodConductivity * denominator;
-
-double biasTotal = 0;
-double[] biases = new double[8];
-
-// Now figure out how wind effects the bias towards each direction.
-for (int i = 0; i < 8; i++) {
-	double differance = temperature;
-	double change = 0;
-	if (Neighbors[i] != null) {
-		differance -= Neighbors[i].temperature;
-	} else {
-		differance -= OOB_Temperature;
-	} 
-	
-	if (differance <= 0) {
-		biases[i] = 0;
-		continue; // cell is same temperature so no need to change it
-	} 
-	
-	if (i % 2 == 0) { 
-		change = differance / denominator;
-	} else { // odd numbers are diagonal and subject to the angleWeight
-		change = angledWeight * differance / denominator;
-	}
-	
-	double angleToNeighbor = i * (Math.PI / 4);
-	
-	biases[i] = 1.0;
-	biases[i] += (get_Main().windSpeed * get_Main().windPower) * Math.cos(angleToNeighbor - get_Main().windDirection);
-	biases[i] = Math.max(0, biases[i]);
-	biases[i] *= change;
-	
-	biasTotal += biases[i];
-}
-if (biasTotal <= 1e-12) {
-	return; // something went wrong and it should stop if this happens.
-}
-
-double lostTemperature = 0;
-
-// Finally apply the temperature change.
-for (int i = 0; i < 8; i++) {	
-	if (biases[i] == 0) {
-		continue; // cell is same temperature so no need to change it
-	} 
-	double change = budget * (biases[i] / biasTotal);
-	change /= get_Main().heatCapacity;
-	
-	if (Neighbors[i] != null) {
-		Neighbors[i].temperature += change;
-	}
-	lostTemperature += change;
-}
-
-//System.out.println("temperature delta: " + lostTemperature);
-//System.out.println("temperature: " + temperature);
-
-// this should never set it to base but it is still a safty percausion
-// for vary weird edge cases
-temperature = Math.max(get_Main().baseTemperature, temperature - lostTemperature);
-
+return vector < Math.pow(burnRadius, 2);
 /*ALCODEEND*/}
 
-double notes()
-{/*ALCODESTART::1763927011571*/
-/* 
-both temperature regulation and fuel consumption is based on
-area of the cell and surface area of the fuel in the cell.
+boolean igniteNeighbor(int i)
+{/*ALCODESTART::1763950567108*/
+double CellX = 0;
+double CellY = 0;
 
-This would require a complete rework of the first half of burning
-to account for surface area of fuel, as well as a update to the
-heat spread system to conduct heat normally through fuel to fuel, air
-to air, and ground to ground.
-*/
+Neighbors[i].isCellBurning = true;
+switch (i) { 
+	case 0:
+		CellX = -1;
+		CellY = 1;
+		break;
+	case 1:
+		CellY = 1;
+		break;
+	case 2:
+		CellX = 1;
+		CellY = 1;
+		break;
+	case 3:
+		CellX = 1;
+		break;
+	case 4:
+		CellX = 1;
+		CellY = -1;
+		break;
+	case 5:
+		CellY = -1;
+		break;
+	case 6:		
+		CellX = -1;
+		CellY = -1;
+		break;
+	case 7:		
+		CellX = -1;
+	break;
+}
+
+Neighbors[i].firePointX = -CellX * get_Main().cellWidth / 2;
+Neighbors[i].firePointY	= -CellY * get_Main().cellWidth / 2;
+
+send("Ignite", Neighbors[i]);
 /*ALCODEEND*/}
 
